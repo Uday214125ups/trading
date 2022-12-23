51fd83a232fd63e943c6ddcefa93bfe34e8f4ad2
Uday <uday214125ups@gmail.com>
2022-12-23 15:29:18 +0530

    .

---
+++ Customer_login.php
@@ -0,0 +1,123 @@
+<html>
+       <head>
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+<link rel="stylesheet" href="style.css">
+
+<link rel="icon" href="skull-solid.svg" type="image/icon type"> 
+
+
+       </head>
+<body>
+      
+    <header>
+
+<div class="header_img">
+
+
+
+<nav class="navbar navbar-expand-lg Navbaar fixed-top">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5 text-center">
+                <li class="nav-item">
+                    <a class="nav-link active" style="color:white;" href="#home">Home</a>
+                  </li>
+                 
+                  
+               
+
+                  <li class="nav-item">
+                    <a class="nav-link active" style="color:white;"  href="#about">About Us</a>
+                  </li>
+              
+                  
+                  <li class="nav-item">
+                    <a class="nav-link" style="color:white;" href="#contact">Contact Us</a>
+                  </li>
+                
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<section class="form my-4 mx-5">
+    <div class="container">
+        <div class="row no-gutters">
+            <center>
+            <div class="col-lg-5 px-5 pt-5">
+                <h4 style="color:white;">Sign into your account</h4> 
+                <form method='post' action='login_check.php'>
+                    <div class="form-row Forrm">
+                        <div class="col-lg-7">
+                            <input type="email" name='fmail' class="form-control my-3 p-3" placeholder="Email-Address / User_id">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="col-lg-7">
+                            <input type="password" name='fpass' class="form-control my-3 p-3" placeholder="*******">
+                        </div>
+                    </div>
+
+                    <div class="form-row">
+                        <div class="col-lg-7">
+                        <a href="customer_login.php"><button class="btn btn-warning btn-lg">Login</button></a>
+                  </div>
+                    </div>
+
+                    <a href="#">Forgot Password ?</a>
+                    <p style="color:white;">Don't have an account? <a href="#">Register here</a></p>
+
+                </form>
+</center>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
+
+</div>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
